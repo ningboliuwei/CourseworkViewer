e9fab5c8c582680d89118e5eae89ea2aea3ea0ec
ningboliuwei <liuwei@nbu.edu.cn>
2015-01-04 17:31:57 +0800

    第一次提交

---
+++ CourseworkViewer/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CourseworkViewer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.文件FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.选择文件夹DToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.Controls.Add(this.menuStrip1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 562);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件FToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// 文件FToolStripMenuItem
+			// 
+			this.文件FToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.选择文件夹DToolStripMenuItem});
+			this.文件FToolStripMenuItem.Name = "文件FToolStripMenuItem";
+			this.文件FToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+			this.文件FToolStripMenuItem.Text = "文件(&F)";
+			// 
+			// 选择文件夹DToolStripMenuItem
+			// 
+			this.选择文件夹DToolStripMenuItem.Name = "选择文件夹DToolStripMenuItem";
+			this.选择文件夹DToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+			this.选择文件夹DToolStripMenuItem.Text = "选择文件夹(&D)";
+			this.选择文件夹DToolStripMenuItem.Click += new System.EventHandler(this.选择文件夹DToolStripMenuItem_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.Location = new System.Drawing.Point(3, 33);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(778, 526);
+			this.dataGridView1.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(784, 562);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "CourseworkViewer";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem 文件FToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem 选择文件夹DToolStripMenuItem;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+	}
+}
+
---
+++ CourseworkViewer/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseworkViewer
+{
+	using System.IO;
+
+	public partial class Form1 : Form
+	{
+		private string scanDirectoryPath = ""; //用户选择的上传文件夹（upload文件夹）
+
+		private List<string> courseworkNameList = new List<string>();
+
+		private List<string> studentNameList = new List<string>();
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void 选择文件夹DToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+			{
+				dataGridView1.DataSource = 0;
+				scanDirectoryPath = folderBrowserDialog1.SelectedPath;
+
+				//scanDirectoryPath = @"E:\HTTP\CourseworkUploadSystem4ASP\";
+
+				this.ReadConfigFile(scanDirectoryPath + "\\config.txt");
+				ShowAllFiles(scanDirectoryPath + "\\upload\\");
+				this.ChangeColor();
+			}
+		}
+
+		private void ShowAllFiles(string directoryPath)
+		{
+			List<DataRow> rows = new List<DataRow>();
+
+
+			try
+			{
+				DataTable filesDataTable = new DataTable("filesDataTable");
+
+				filesDataTable.Columns.Add("序号");
+				filesDataTable.Columns.Add("学号");
+				filesDataTable.Columns.Add("姓名");
+				filesDataTable.Columns.Add("该文件所属的作业名称");
+				filesDataTable.Columns.Add("文件提交时间");
+				filesDataTable.Columns.Add("提交的文件名");
+
+				foreach (string studentName in studentNameList)
+				{
+					foreach (string courseworkName in courseworkNameList)
+					{
+						DataRow datarow = filesDataTable.NewRow();
+						datarow[1] = studentName.Split('+')[0];
+						datarow[2] = studentName.Split('+')[1];
+						datarow[3] = courseworkName;
+
+						filesDataTable.Rows.Add(datarow);
+					}
+				}
+
+				//filesDataTable.DefaultView.Sort = "学号 ASC, 该文件所属的作业名称 ASC";
+
+				//directoryPath 就是那个选中的“upload”文件夹
+				string[] courseworkPathList = Directory.GetDirectories(directoryPath); //每项就是结尾是每次作业的绝对路径
+
+				for (int i = 0; i < courseworkPathList.Length; i++) //遍历upload下每个作业文件夹
+				{
+					string currentCourseworkPath = courseworkPathList[i];
+					string currentCourseworkName = currentCourseworkPath.Substring(currentCourseworkPath.LastIndexOf("\\") + 1);
+					//获取当前作业名称
+
+					string[] studentPathList = Directory.GetDirectories(currentCourseworkPath); //得到当前作业文件夹下所有学生的文件夹绝对路径（学号+姓名）
+
+					for (int j = 0; j < studentPathList.Length; j++) //遍历某个作业文件夹中每个学生文件夹
+					{
+						string currentStudentPath = studentPathList[j];
+						string currentStudentName = currentStudentPath.Substring(currentStudentPath.LastIndexOf("\\") + 1);
+
+						foreach (DataRow currentRow in filesDataTable.Rows)
+						{
+							if ((currentRow[1] + "+" + currentRow[2]) == currentStudentName && currentRow[3].ToString() == currentCourseworkName)
+							{
+								string[] courseworkFileList = Directory.GetFiles(currentStudentPath);
+
+								for (int k = 0; k < courseworkFileList.Length; k++)
+								{
+									string currentCourseworkFilePath = courseworkFileList[k];
+									string currentCourseworkFileName =
+										currentCourseworkFilePath.Substring(currentCourseworkFilePath.LastIndexOf("\\") + 1);
+
+									if (k == 0)
+									{
+										//currentRow[0] = Convert.ToString(filesDataTable.Rows. + 1);//序号
+										//currentRow[3] = currentCourseworkName; //所属作业名称
+										currentRow[5] = currentCourseworkFileName; //提交的文件名
+										//currentRow[1] = currentCourseworkFileName.Split('+')[0]; //学号
+										//currentRow[2] = currentCourseworkFileName.Split('+')[1]; //姓名
+										currentRow[4] = currentCourseworkFileName.Split('+')[2]; //提交时间
+									}
+									else
+									{
+										DataRow sameRow = filesDataTable.NewRow(); //同一个作业超过提交一个文件
+										sameRow[0] = currentRow[0];
+										sameRow[1] = currentRow[1];
+										sameRow[2] = currentRow[2];
+										sameRow[3] = currentRow[3];
+										sameRow[4] = currentCourseworkFileName.Split('+')[2];
+										sameRow[5] = currentCourseworkFileName;
+
+										//filesDataTable.Rows.Add(sameRow);
+										rows.Add(sameRow);
+									}
+									//filesDataTable.Rows.Add(currentRow);
+								}
+							}
+						}
+					}
+				}
+
+				foreach (DataRow row in rows)
+				{
+					filesDataTable.Rows.Add(row);
+				}
+
+				filesDataTable.DefaultView.Sort = "学号 ASC, 该文件所属的作业名称 ASC, 文件提交时间 ASC";
+				dataGridView1.DataSource = filesDataTable.DefaultView;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+
+		private void ChangeColor()
+		{
+			List<Color> colorList = new List<Color>();
+			colorList.Add(Color.LightBlue);
+			colorList.Add(Color.LightCoral);
+			colorList.Add(Color.LightPink);
+			colorList.Add(Color.LightGreen);
+
+
+			int colorIndex = 0;
+			dataGridView1.Rows[0].DefaultCellStyle.BackColor = colorList[colorIndex];
+			int no = 1;
+
+			dataGridView1.Rows[0].Cells[0].Value = no;
+
+			for (int i = 1; i < dataGridView1.Rows.Count-1; i++)
+			{
+				no++;
+				if (dataGridView1.Rows[i].Cells[1].Value.ToString() != dataGridView1.Rows[i - 1].Cells[1].Value.ToString())
+				{
+					no = 1;
+					colorIndex++;
+
+					if (colorIndex > 3)
+					{
+						colorIndex = 0;
+					}
+
+
+
+					
+					dataGridView1.Rows[i].Cells[0].Value = no;
+				}
+				else
+				{
+					//dataGridView1.Rows[i].DefaultCellStyle.BackColor = rowColor;
+					dataGridView1.Rows[i].Cells[0].Value = no;
+				}
+				dataGridView1.Rows[i].DefaultCellStyle.BackColor = colorList[colorIndex]; ;
+			}
+		}
+
+		//public Color GetRandomColor()
+		//{
+		//	Random RandomNum_First = new Random(Guid.NewGuid().GetHashCode());
+		//	//  对于C#的随机数，没什么好说的
+		//	//System.Threading.Thread.Sleep(RandomNum_First.Next(50));
+		//	Random RandomNum_Sencond = new Random(Guid.NewGuid().GetHashCode());
+			
+		//	//System.Threading.Thread.Sleep(RandomNum_First.Next(50));
+		//	Random RandomNum_Third = new Random(Guid.NewGuid().GetHashCode());
+
+		//	//  为了在白色背景上显示，尽量生成深色
+		//	int int_Red = RandomNum_First.Next(256);
+		//	int int_Green = RandomNum_Sencond.Next(256);
+		//	//int int_Blue = (int_Red + int_Green < 400) ? 0 : 400 - int_Red - int_Green;
+		//	int int_Blue = RandomNum_Sencond.Next(256);
+			
+		//	//int_Blue = (int_Blue > 255) ? 255 : int_Blue;
+
+		//	return Color.FromArgb(int_Red, int_Green, int_Blue);
+		//}
+
+
+
+		private void ReadConfigFile(string configFilePath)
+		{
+			StreamReader sr = null;
+
+			try
+			{
+				sr = new StreamReader(configFilePath, Encoding.Default); //得到文件路径并读取
+
+				string currentLine = "";
+
+				while ((currentLine = sr.ReadLine()) != null)
+				{
+					if (currentLine == "[Student List]")
+					{
+						string studentRecordStr = "";
+
+						while (studentRecordStr != "[Coursework List]")
+						{
+							studentRecordStr = sr.ReadLine().Trim();
+							string[] studentRecord = new string[2];
+
+							if (studentRecordStr != "[Coursework List]")
+							{
+								studentRecord = studentRecordStr.Split(',');
+								studentNameList.Add(studentRecord[0] + "+" + studentRecord[1]);
+							}
+						}
+
+						while (sr.Peek() >= 0)
+						{
+							studentRecordStr = sr.ReadLine().Trim();
+							courseworkNameList.Insert(0, studentRecordStr);
+						}
+					}
+					else
+					{
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+			finally
+			{
+				if (sr != null)
+				{
+					sr.Close();
+				}
+			}
+		}
+	}
+}
